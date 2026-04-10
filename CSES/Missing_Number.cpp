f9ffe54007e3f9bbbfd115590f0f9e6585bd6b68
unknown <mintrikfcode@gmail.com>
2026-04-10 22:02:49 +0700

    [CSES] Missing_Number.cpp

--- CSES/Missing_Number.cpp
+++ CSES/Missing_Number.cpp
@@ -2,14 +2,29 @@
 // Created: Minh Triet
 // Date: 07/04/2026
 
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 2e5 + 5;
+
 void solve()
 {
+    int n, a[N];
+    cin >> n;
+    for(int i = 1 ; i <= n - 1; i ++)
+        cin >> a[i];
 
+    sort(a, a + n);
+
+    for(int i = 1 ; i <= n - 1; i ++)
+        if(a[i] != i)
+        { 
+            cout << a[i] - 1;
+            return;
+        }
+    cout << n;
 }
 
 signed main()
